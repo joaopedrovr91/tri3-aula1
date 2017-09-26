c54df216bd882ade71a1cdca07da967f4a94eccf
Gabriel Alves <gabrielalves199816@gmail.com>
2017-09-26 11:27:55 -0300

    Projeto Iniciado

---
+++ Assets/Scripts/MainMenuUICtrl.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUICtrl : MonoBehaviour {
+
+	public void LoadScene(string SceneName) {
+		SceneManager.LoadScene(SceneName);
+	}
+}
