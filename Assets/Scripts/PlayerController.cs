4afbb61dac97a292640f0ca4b8cdd68b9493102c
joaopedro <joaopedrovr91@gmail.com>
2017-10-31 12:12:50 -0200

    Die & Respawn

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -36,6 +36,10 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		if(transform.position.y <GM.instance.yMinLive){
+			GM.instance.KillPlayer();
+		}
+
 		isGrounded = Physics2D.OverlapBox(new Vector2(feet.position.x,feet.position.y), new Vector2(feetWidth, feetHeight), 360.0f, whatIsGround);
 		
 		float horizontalInput = Input.GetAxisRaw("Horizontal");
