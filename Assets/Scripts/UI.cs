521bce2b03133482c6ba26b706b69c75a26b3b50
joaopedrovr <joaopedrovr91@gmail.com>
2017-11-21 11:58:55 -0200

    Preparando o painel de gamer over

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -24,7 +24,22 @@ public class UI {
 		public GameObject hudPanel;
 	}
 
+	[Serializable]
+	public class GameOver {
+
+		[Header("Text")]
+	
+		public Text txtCoinCount;
+
+		public Text txtTimer;
+
+		[Header("Other")]
+
+		public GameObject GameOverPanel;
+	}
+
 	public HUD hud;
+	public GameOver gameOver;
 }
 
 
